7e97a66baabcc8cc5bfab8d54ca4f59fda01fa63
Kostas Demiris <kostas@holograpgh.xyz>
2025-03-14 18:23:30 +0200

    updated compose

--- scripts/init/init-aws-localstack.sh
+++ scripts/init/init-aws-localstack.sh
@@ -7,4 +7,8 @@ awslocal sqs create-queue --queue-name q1
 awslocal secretsmanager create-secret \
     --name all-secrets \
     --description "LocalStack Secrets" \
-    --secret-string /secrets.json
+    --secret-string '{"username": "admin", "password": "password"}'
+
+#
+awslocal kms create-key \
+  --tags '[{"TagKey":"_custom_id_","TagValue":"00000000-0000-0000-0000-000000000001"}]'
